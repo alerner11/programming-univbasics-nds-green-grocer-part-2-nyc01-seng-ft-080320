173b76cf5f0dd0e7bbb3c0cf18ef1875e4b54f98
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-22 16:04:45 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,6 +4,45 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  cart_with_coupons = []
+  
+  cart.each do |item_hash|
+    # store whether there's a coupon for ease of typing later
+    # returns coupon hash if there is a coupon, nil if not
+    has_coupon = find_item_by_name_in_collection(item_hash[:item], coupons)
+    
+    # if the item has a coupon 
+    # then transfer the appropriate number of items to new W/COUPON item hash
+    # including cost per each in that W/COUPON item hash
+    if has_coupon
+      
+      # check if there's enough in the cart to qualify for the coupon
+      if item_hash[:count] >= has_coupon[:num] 
+        
+        # if it doesn't divide evenly, add coupon-less item to cart_with_coupons, update count to = remainder, subtract remainder from item_hash[:count] 
+        if item_hash[:count] % has_coupon[:num] > 0
+           cart_with_coupons << item_hash
+           cart_with_coupons[-1][:count] = item_hash[:count] % has_coupon[:num]
+           item_hash[:count] = item_hash[:count] - cart_with_coupons[-1][:count]
+           puts item_hash[:count]
+        end
+        
+        # now they definitely divide evenly
+        cart_with_coupons << {
+          item: "#{item_hash[:item]} W/COUPON",
+          price: has_coupon[:cost].to_f / has_coupon[:num].to_f,
+          clearance: item_hash[:clearance],
+          count: item_hash[:count]
+        }
+      end
+      
+    # if there is no coupon, just shovel it into coupon_cart as is
+    else
+      cart_with_coupons << item_hash
+    end
+  end
+  puts cart_with_coupons
 end
 
 def apply_clearance(cart)
@@ -23,3 +62,14 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
 end
+
+cons_cart = [
+  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 3},
+  {:item => "KALE",    :price => 3.00, :clearance => false, :count => 1}
+]
+
+coupons_list = [
+  {:item => "AVOCADO", :num => 2, :cost => 5.00}
+]
+
+apply_coupons(cons_cart, coupons_list)
