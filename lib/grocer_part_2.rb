146c94c2528c9f5dfaa8b93f4e129d7262559d72
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-22 16:20:59 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -20,10 +20,9 @@ def apply_coupons(cart, coupons)
       # check if there's enough in the cart to qualify for the coupon
       if item_hash[:count] >= has_coupon[:num] 
         orig_count = item_hash[:count]
-        remainder = 0
+        remainder = item_hash[:count] % has_coupon[:num]
         # if it doesn't divide evenly, add coupon-less item to cart_with_coupons, update count for coupon-less item to equal remainder 
-        if item_hash[:count] % has_coupon[:num] > 0
-          remainder = item_hash[:count] % has_coupon[:num]
+        if remainder > 0
           cart_with_coupons << item_hash
           cart_with_coupons[-1][:count] = remainder
         end
